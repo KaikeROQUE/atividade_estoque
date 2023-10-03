52bda1df33ced9bc46702d60ab3b39924a26335f
KaikeROQUE <107213901+KaikeROQUE@users.noreply.github.com>
2023-10-02 21:01:46 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'estoque';
+    
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+?>
